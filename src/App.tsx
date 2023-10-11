37f98f0d14982cb377e0f9a95ec0cfaeeab69f5d
mikii17 <114937644+mikii17@users.noreply.github.com>
2023-10-11 22:32:08 +0300

    Setup project with vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA70NRE04JpqY1Bc7QuwXnJ2XmlaxO5bH4",
+  authDomain: "online-tic-tac-toe-67fc2.firebaseapp.com",
+  projectId: "online-tic-tac-toe-67fc2",
+  storageBucket: "online-tic-tac-toe-67fc2.appspot.com",
+  messagingSenderId: "274898805202",
+  appId: "1:274898805202:web:57ea3c6a46d8cc3f0d280b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
