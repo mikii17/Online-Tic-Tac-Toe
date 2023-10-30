3bc4bdbccd1f739743a5dc5142b6a8ae41fa8ca3
mikii17 <114937644+mikii17@users.noreply.github.com>
2023-10-31 00:51:49 +0300

    Update config

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -6,22 +6,15 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA70NRE04JpqY1Bc7QuwXnJ2XmlaxO5bH4",
-//   authDomain: "online-tic-tac-toe-67fc2.firebaseapp.com",
-//   projectId: "online-tic-tac-toe-67fc2",
-//   storageBucket: "online-tic-tac-toe-67fc2.appspot.com",
-//   messagingSenderId: "274898805202",
-//   appId: "1:274898805202:web:57ea3c6a46d8cc3f0d280b",
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCUVKeOYiunB4J4U7UaE2EqHJ3M0cKnWNo",
-  authDomain: "online-tic-tac-toe-19f15.firebaseapp.com",
-  projectId: "online-tic-tac-toe-19f15",
-  storageBucket: "online-tic-tac-toe-19f15.appspot.com",
-  messagingSenderId: "640569492997",
-  appId: "1:640569492997:web:cef28fec7599dd80df7e6c"
+  apiKey: "AIzaSyA70NRE04JpqY1Bc7QuwXnJ2XmlaxO5bH4",
+  authDomain: "online-tic-tac-toe-67fc2.firebaseapp.com",
+  projectId: "online-tic-tac-toe-67fc2",
+  storageBucket: "online-tic-tac-toe-67fc2.appspot.com",
+  messagingSenderId: "274898805202",
+  appId: "1:274898805202:web:57ea3c6a46d8cc3f0d280b",
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
